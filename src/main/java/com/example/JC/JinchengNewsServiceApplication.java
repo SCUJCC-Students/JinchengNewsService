4326789c238a863e2b446a86661070b1727e56fd
Bernie <bernie@gmail.com>
2018-05-10 00:06:33 +0800

    创建 项目

---
+++ src/main/java/com/example/JC/JinchengNewsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.JC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinchengNewsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinchengNewsServiceApplication.class, args);
+	}
+}
